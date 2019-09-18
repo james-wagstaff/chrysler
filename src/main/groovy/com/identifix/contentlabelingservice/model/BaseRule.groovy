7ad4323e317c47b443f7f7e2f64f8db8c10600ab
Moseng, Michael (Identifix) <Michael.Moseng@identifix.com>
2019-09-18 10:08:59 -0500

    removed getters/setters

--- src/main/groovy/com/identifix/contentlabelingservice/model/BaseRule.groovy
+++ src/main/groovy/com/identifix/contentlabelingservice/model/BaseRule.groovy
@@ -1,33 +1,9 @@
 package com.identifix.contentlabelingservice.model
 
 class BaseRule {
-    private BaseRuleType type
-    private String regexWords
-    private String rule
-
-    String getRegexWords() {
-        return regexWords
-    }
-
-    void setRegexWords(String regexWords) {
-        this.regexWords = regexWords
-    }
-
-    String getRule() {
-        return rule
-    }
-
-    void setRule(String rule) {
-        this.rule = rule
-    }
-
-    BaseRuleType getType() {
-        return type
-    }
-
-    void setType(BaseRuleType type) {
-        this.type = type
-    }
+    BaseRuleType type
+    String regexWords
+    String rule
 }
 
 enum BaseRuleType {
--- src/main/groovy/com/identifix/contentlabelingservice/model/LabelRequest.groovy
+++ src/main/groovy/com/identifix/contentlabelingservice/model/LabelRequest.groovy
@@ -6,43 +6,11 @@ import io.swagger.annotations.ApiModelProperty
 @ApiModel(description = "All details needed to find a label through base rules.")
 class LabelRequest {
     @ApiModelProperty(example = "Ford")
-    private String publisher
+    String publisher
     @ApiModelProperty(example = "Workshop")
-    private String manualType
+    String manualType
     @ApiModelProperty(example = "Removal and Installation: Axle Assembly - (Front Drive Axle/Differential)")
-    private String title
-    @ApiModelProperty(example = "[113 > 2: Chassis > 05: Driveline > 205-03 Front Drive Axle/Differential > Removal and Installation > Axle Assembly]")
-    private String tocPath
-
-    String getPublisher() {
-        return publisher
-    }
-
-    void setPublisher(String publisher) {
-        this.publisher = publisher
-    }
-
-    String getManualType() {
-        return manualType
-    }
-
-    void setManualType(String manualType) {
-        this.manualType = manualType
-    }
-
-    String getTitle() {
-        return title
-    }
-
-    void setTitle(String title) {
-        this.title = title
-    }
-
-    String getTocPath() {
-        return tocPath
-    }
-
-    void setTocPath(String tocPath) {
-        this.tocPath = tocPath
-    }
+    String title
+    @ApiModelProperty(example = "\"[113 > 2: Chassis > 05: Driveline > 205-03 Front Drive Axle/Differential > Removal and Installation > Axle Assembly]\"")
+    String tocPath
 }
