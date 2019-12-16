0d29c58722370d882e9d1a0a6ec35a54e9c82414
Lance Saxerud <Lance.Saxerud@identifix.com>
2019-12-16 14:07:39 -0600

    intial commit, adding in process for Wiring Manuals

--- src/test/groovy/com/identifix/contentlabelingservice/web/LabelControllerSpec.groovy
+++ src/test/groovy/com/identifix/contentlabelingservice/web/LabelControllerSpec.groovy
@@ -12,57 +12,94 @@
 //class LabelControllerSpec extends Specification {
 //    @Autowired
 //    LabelController controller
+////
+////    def 'Apply labels to PCED spreadsheet functional test' () {
+////        when:
+////            int totalDocs = 0
+////            int totalLabelsFound = 0
+////
+////            StringBuilder labelSpreadsheet = new StringBuilder("Label,title,tocpath,category,linkToPage,nuxeoId\r\n")
+////            String originalCSV = new File("src/test/resources/documentTable.csv").text
+////            originalCSV.split("\\r?\\n").drop(1).each {
+////                totalDocs++
+////                Document document = createDocument(it)
+////                LabelRequest request = new LabelRequest()
+////                request.publisher = "Ford"
+////                request.manualType = "PCED" //document.category
+////                request.title = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[3]
+////                request.header = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[2]
+////                ResponseEntity responseEntity = controller.createLabel(request)
+////                document.label = responseEntity.statusCode == HttpStatus.OK ? responseEntity.body.toString() : "Not Found"
+////
+////                if (responseEntity.statusCode == HttpStatus.OK) {
+////                    totalLabelsFound++
+////                }
+////
+////                labelSpreadsheet.append("${document.label},${document.title},${document.tocpath},${document.category},${document.linkToPage},${document.nuxeoId}\r\n")
+////            }
+////
+////            println ("${((totalLabelsFound / totalDocs) * 100).round(2)} % labeled")
+////
+////            File file = new File("src/test/resources/documentTable_finished.csv")
+////            PrintWriter writer = new PrintWriter(file)
+////            writer.print("")
+////            file.append(labelSpreadsheet.toString().bytes)
+////            file.createNewFile()
+////
+////        then:
+////            true
+////    }
 //
-//    def 'Apply labels to PCED spreadsheet functional test' () {
-//        when:
-//            int totalDocs = 0
-//            int totalLabelsFound = 0
-//
-//            StringBuilder labelSpreadsheet = new StringBuilder("Label,title,tocpath,category,linkToPage,nuxeoId\r\n")
-//            String originalCSV = new File("src/test/resources/documentTable.csv").text
-//            originalCSV.split("\\r?\\n").drop(1).each {
-//                totalDocs++
-//                Document document = createDocument(it)
-//                LabelRequest request = new LabelRequest()
-//                request.publisher = "Ford"
-//                request.manualType = "PCED" //document.category
-//                request.title = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[3]
-//                request.header = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[2]
-//                ResponseEntity responseEntity = controller.createLabel(request)
-//                document.label = responseEntity.statusCode == HttpStatus.OK ? responseEntity.body.toString() : "Not Found"
-//
-//                if (responseEntity.statusCode == HttpStatus.OK) {
-//                    totalLabelsFound++
-//                }
-//
-//                labelSpreadsheet.append("${document.label},${document.title},${document.tocpath},${document.category},${document.linkToPage},${document.nuxeoId}\r\n")
-//            }
-//
-//            println ("${((totalLabelsFound / totalDocs) * 100).round(2)} % labeled")
-//
-//            File file = new File("src/test/resources/documentTable_finished.csv")
-//            PrintWriter writer = new PrintWriter(file)
-//            writer.print("")
-//            file.append(labelSpreadsheet.toString().bytes)
-//            file.createNewFile()
-//
-//        then:
-//            true
-//    }
-//
-//    def 'Apply labels to Workshop spreadsheet functional test' () {
+////    def 'Apply labels to Workshop spreadsheet functional test' () {
+////        when:
+////        int totalDocs = 0
+////        int totalLabelsFound = 0
+////
+////        StringBuilder labelSpreadsheet = new StringBuilder("Label,title,tocpath,category,linkToPage,nuxeoId\r\n")
+////        String originalCSV = new File("src/test/resources/documentTable.csv").text
+////        originalCSV.split("\\r?\\n").drop(1).each {
+////            totalDocs++
+////            Document document = createDocument(it)
+////            LabelRequest request = new LabelRequest()
+////            request.publisher = "Ford"
+////            request.manualType = "Workshop" //document.category
+////            request.title = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[5]
+////            request.header = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[4]
+////            ResponseEntity responseEntity = controller.createLabel(request)
+////            document.label = responseEntity.statusCode == HttpStatus.OK ? responseEntity.body.toString() : "Not Found"
+////
+////            if (responseEntity.statusCode == HttpStatus.OK) {
+////                totalLabelsFound++
+////            }
+////
+////            labelSpreadsheet.append("${document.label},${document.title},${document.tocpath},${document.category},${document.linkToPage},${document.nuxeoId}\r\n")
+////        }
+////
+////        println ("${((totalLabelsFound / totalDocs) * 100).round(2)} % labeled")
+////
+////        File file = new File("src/test/resources/documentTable_finished.csv")
+////        PrintWriter writer = new PrintWriter(file)
+////        writer.print("")
+////        file.append(labelSpreadsheet.toString().bytes)
+////        file.createNewFile()
+////
+////        then:
+////        true
+////    }
+////TODO rename source and output files accordingly before running test
+//    def 'Apply labels to Wiring spreadsheet functional test' () {
 //        when:
 //        int totalDocs = 0
 //        int totalLabelsFound = 0
 //
 //        StringBuilder labelSpreadsheet = new StringBuilder("Label,title,tocpath,category,linkToPage,nuxeoId\r\n")
-//        String originalCSV = new File("src/test/resources/documentTable.csv").text
+//        String originalCSV = new File("src/test/resources/rename_source_file_here.csv").text
 //        originalCSV.split("\\r?\\n").drop(1).each {
 //            totalDocs++
 //            Document document = createDocument(it)
 //            LabelRequest request = new LabelRequest()
 //            request.publisher = "Ford"
-//            request.manualType = "Workshop" //document.category
+//            request.manualType = "Wiring" //document.category
 //            request.title = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[5]
 //            request.header = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[4]
 //            ResponseEntity responseEntity = controller.createLabel(request)
@@ -77,7 +114,7 @@
 //
 //        println ("${((totalLabelsFound / totalDocs) * 100).round(2)} % labeled")
 //
-//        File file = new File("src/test/resources/documentTable_finished.csv")
+//        File file = new File("src/test/resources/rename_output_file_here.csv")
 //        PrintWriter writer = new PrintWriter(file)
 //        writer.print("")
 //        file.append(labelSpreadsheet.toString().bytes)
