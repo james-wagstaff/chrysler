77e57ebfb743503d3d8eead001a4fe638799d8cf
Moseng, Michael (Identifix) <Michael.Moseng@identifix.com>
2020-10-28 13:46:59 -0500

    added workshop test back in

--- src/test/groovy/com/identifix/contentlabelingservice/web/LabelControllerSpec.groovy
+++ src/test/groovy/com/identifix/contentlabelingservice/web/LabelControllerSpec.groovy
@@ -52,6 +52,42 @@
 ////            true
 ////    }
 ////
+////    def 'Apply labels to Workshop spreadsheet functional test' () {
+////        when:
+////        int totalDocs = 0
+////        int totalLabelsFound = 0
+////
+////        StringBuilder labelSpreadsheet = new StringBuilder("Label,title,tocpath,category,linkToPage,nuxeoId\r\n")
+////        String originalCSV = new File("src/test/resources/documentTable.csv").text
+////        originalCSV.split("\\r?\\n").drop(1).each {
+////            totalDocs++
+////            Document document = createDocument(it)
+////            LabelRequest request = new LabelRequest()
+////            request.publisher = "Ford"
+////            request.manualType = "Workshop" //document.category
+////            request.title = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ").last()
+////            request.header = document.tocpath.replaceAll("\\[", "").replaceAll("]", "").split(" > ")[4]
+////            ResponseEntity responseEntity = controller.createLabel(request)
+////            document.label = responseEntity.statusCode == HttpStatus.OK ? responseEntity.body.toString() : "Not Found"
+////
+////            if (responseEntity.statusCode == HttpStatus.OK) {
+////                totalLabelsFound++
+////            }
+////
+////            labelSpreadsheet.append("${document.label},${document.title},${document.tocpath},${document.category},${document.linkToPage},${document.nuxeoId}\r\n")
+////        }
+////
+////        println ("${((totalLabelsFound / totalDocs) * 100).round(2)} % labeled")
+////
+////        File file = new File("src/test/resources/documentTable_finished.csv")
+////        PrintWriter writer = new PrintWriter(file)
+////        writer.print("")
+////        file.append(labelSpreadsheet.toString().bytes)
+////        file.createNewFile()
+////
+////        then:
+////        true
+////    }
 //    def 'Apply labels to Repair Manual spreadsheet functional test' () {
 //        when:
 //        int totalDocs = 0
