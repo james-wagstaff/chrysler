abbcc34d1e3ea4406f88948d7b779448eb0a84a3
beyaadamu <beya.adamu@identifix.com>
2020-12-28 23:13:06 -0600

    RAP2991: add chrysler labeler.

---
+++ src/main/groovy/com/identifix/contentlabelingservice/utils/CommonConstants.groovy
@@ -0,0 +1,13 @@
+package com.identifix.contentlabelingservice.utils
+
+class CommonConstants {
+
+    static final String ITEM = "item"
+    static final String PAGECODE = "pageCode"
+    static final String TITLE = "title"
+    static final String PUBLISHER_CHRYSLER =  "Chrysler"
+    static final String MANUAL_TYPE_SERVICE =  "Service"
+
+    static final String REGEX_REPEATING_WORD = "\\b(\\w+)(?:\\W+\\1\\b)+"
+
+}
---
+++ src/test/groovy/com/identifix/contentlabelingservice/label/chrysler/ServiceInfoLabelMakerSpec.groovy
@@ -0,0 +1,71 @@
+package com.identifix.contentlabelingservice.label.chrysler
+
+import com.identifix.contentlabelingservice.label.LabelMakerMessage
+import com.identifix.contentlabelingservice.model.LabelRequest
+import com.identifix.contentlabelingservice.service.GitService
+import com.identifix.contentlabelingservice.service.LabelService
+import com.identifix.crawlermqutils.handler.MessageHandlerResponse
+import com.identifix.kraken.client.KrakenClient
+import com.identifix.kraken.client.bean.Manual
+import spock.lang.Specification
+
+class ServiceInfoLabelMakerSpec extends Specification {
+
+    ServiceInfoLabelMaker systemUnderTest = new ServiceInfoLabelMaker()
+    KrakenClient mockKrakenClient = Mock(KrakenClient)
+    LabelService mockLabelService = Mock(LabelService)
+    GitService mockGitService = Mock(GitService)
+
+    void setup() {
+        systemUnderTest.krakenClient = mockKrakenClient
+        systemUnderTest.labelService = mockLabelService
+        systemUnderTest.gitService = mockGitService
+    }
+
+    def "test label Content"() {
+        given:
+            LabelMakerMessage message = new ChryslerServiceInfoMessage(crawlerTypeKey:"CHRYSLER_SERVICE_LABEL", manualType:"Service", year:"2018", model:"Jeep Renegade", manualId:"549859cd-7c58-3270-a0e0-56806b022b69")
+            List<Manual> manuals = [ new Manual(publisherManualCategory:"service", publisherDocumentId:"1", title:"Jeep")]
+            String label = "label1,title1,to> cpa >th1,category1,linkToPage1,nuxeoId1\n\rlabel2,title2,to > cpa> th2,category2,linkToPage2,nuxeoId2"
+        when:
+            MessageHandlerResponse result = systemUnderTest.labelContent(message)
+        then:
+            1 * mockKrakenClient.getManuals('2018', 'Jeep Renegade', null) >>  manuals
+            1 * mockKrakenClient.getManualBytes(_ as Manual) >> "content1".bytes
+            1 * mockKrakenClient.buildChryslerLabelingCsv('2018', 'Jeep Renegade', [91, 93], 'Service') >> label
+            1 * mockLabelService.createLabel(_ as LabelRequest) >> "label"
+            1 * mockGitService.uploadCsv(_, _, _, _)
+            0 * _
+        and:
+            result
+    }
+
+    def "test remove Duplicate Words"() {
+        given:
+            String input = "Jeep Jeep Renegade"
+        when:
+            String  result = systemUnderTest.removeDuplicateWords(input)
+        then:
+            0 * _
+        and:
+            result == "Jeep Renegade"
+    }
+
+    def "test to JSONArray Byte byte[] content"() {
+        given:
+        byte[] content = new String(
+                "<item catnbr='00' id='1' title='00 - Vehicle Data '> \n" +
+                "    <item catnbr='0001' id='2' title='Vehicle Information '> \n" +
+                "        <item compid='00013' id='3' pageCode='c82081b4-a8f0-4e76-b6a8-ca4bd89c32bf' title='International Vehicle Control and Display Symbols Description'> </item> \n" +
+                "        <item compid='00025' id='4' pageCode='251126b0-fdcd-4aa6-bf64-3feda024faaa' title='LABEL, Vehicle Emission Certification Information (VECI) Description'> </item> \n" +
+                "        <item compid='00027' id='5' pageCode='3cfc4dbc-58f2-484e-afbb-b1a6c1e2de85' title='Vehicle Certification Label Description'> </item> \n" +
+                "    </item>\n" +
+                "</item>").bytes
+        when:
+            byte[] result = systemUnderTest.toJSONArrayByte(content)
+        then:
+            0 * _
+        and:
+            result
+    }
+}
