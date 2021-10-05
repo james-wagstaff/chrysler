ca97fea251b4b352a5e001c0b14a381ef51fe738
alelupio <alejandra.lupio@identifix.com>
2021-10-04 19:24:07 -0600

    Updated for WiringToc

--- src/main/groovy/com/identifix/contentlabelingservice/utils/TocConverter.groovy
+++ src/main/groovy/com/identifix/contentlabelingservice/utils/TocConverter.groovy
@@ -2,17 +2,25 @@ package com.identifix.contentlabelingservice.utils
 
 import org.json.JSONArray
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.jsoup.parser.Parser
 
 class TocConverter {
 
     static byte[] tocXmlToJson(byte[] content) {
-        org.jsoup.nodes.Document document = Jsoup.parse(new String(content), CommonConstants.UTF_8, Parser.xmlParser())
+        Document document = Jsoup.parse(new String(content), CommonConstants.UTF_8, Parser.xmlParser())
         JSONArray array = new JSONArray()
         document.select(CommonConstants.ITEM).each { item ->
             if (item.hasAttr(CommonConstants.PAGECODE)) {
                 JSONArray object = new JSONArray()
                 object.put(item.attr(CommonConstants.PAGECODE))
+                object.put(item.attr(CommonConstants.TITLE).trim())
+                String tocPath = item.parent().attr(CommonConstants.TITLE)
+                object.put(tocPath)
+                array.put(object)
+            } else if (item.hasAttr(CommonConstants.FILE)) {
+                JSONArray object = new JSONArray()
+                object.put(item.attr(CommonConstants.FILE))
                 object.put(item.attr(CommonConstants.TITLE).trim())
                 String tocPath = item.parent().attr(CommonConstants.TITLE)
                 object.put(tocPath)
