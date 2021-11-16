5ad7d37ff96409331d4c0fb88460b51bed1dc0f7
James.Wagstaff <james.wagstaff@audatex.com>
2021-11-16 10:57:35 -0600

    Add controller for labelling specific vehicles

--- src/main/groovy/com/identifix/contentlabelingservice/web/ServiceBulletinController.groovy
+++ src/main/groovy/com/identifix/contentlabelingservice/web/ServiceBulletinController.groovy
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SuppressWarnings('UnnecessaryGetter')
@@ -32,6 +33,11 @@ class ServiceBulletinController {
     ResponseEntity<Map<String, List<String>>> labelAllVehicles() {
         String vehicleListString = getAllVehiclesText()
         List<VehicleDto> vehicles = parseVehicleList(vehicleListString)
+        processVehicles(vehicles)
+    }
+
+    @GetMapping('/label-specific-vehicles')
+    ResponseEntity<Map<String, List<String>>> labelSpecificVehicles(@RequestParam List<String> vehicles) {
         processVehicles(vehicles)
     }
 
